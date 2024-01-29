07a1bf24cfadeafbaeb82388e9da5511c0a5f4f4
edisonjbernal <edisonjbernal@gmail.com>
2024-01-29 07:42:30 -0500

    uso del Método Post

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -11,8 +11,16 @@ movies = [
 		"title": "En busca de la Felicidad",
 		"overview": "La vida es una lucha para Chris Gardner. Expulsado de su apartamento, él y su joven hijo se encuentran solos sin ningún lugar a donde ir. A pesar de que Chris ocasionalmente consigue trabajo como interno en una prestigiada firma financiera, la posición no le da dinero. El dúo debe vivir en un albergue y enfrentar muchas dificultades, pero Chris no se da por vencido y lucha por conseguir una vida mejor para él y su hijo. Al final logra convertirse en un hombre multimillonario",
 		"year": "2006",
-		"rating": 7.8,
+		"rating": 9.2,
 		"category": "Drama"
+	},
+    {
+		"id": 2,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
 	}
 ]
 
@@ -22,4 +30,28 @@ def message():
 
 @app.get('/movies', tags=['movies'])
 def get_movies():
+    return movies
+
+@app.get('/movies/{id}', tags=['movies'])
+def get_movie(id: int):
+    for item in movies:
+        if item["id"] == id:
+            return item
+    return []
+
+# Parámetro Query Se le agregar una barra al final, para diferenciarlo de get_movies
+@app.get('/movies/', tags=['movies'])
+def get_movies_by_category(category: str, year: int):
+    return [ item for item in movies if item['category'] == category ]
+
+@app.post('/movies', tags=['movies'])
+def create_movie(id: int = Body(), title: str = Body(), overview:str = Body(), year:int = Body(), rating: float = Body(), category: str = Body()):
+    movies.append({
+        "id": id,
+        "title": title,
+        "overview": overview,
+        "year": year,
+        "rating": rating,
+        "category": category
+    })
     return movies
