bb11e1558c46062a113ac3a464819505a9f84964
edisonjbernal <edisonjbernal@gmail.com>
2024-02-02 10:40:33 -0500

    Parámetros de configuración de la base de datos

---
+++ config/database.py
@@ -0,0 +1,16 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+# SQLite
+
+sqlite_file_name = "database.sqlite"
+base_dir = os.path.dirname(os.path.abspath(__file__))
+database_path = os.path.dirname(os.path.realpath(__file__))
+database_url = "sqlite:///" + os.path.join(database_path, sqlite_file_name)
+engine = create_engine(database_url, echo=True)
+
+Session = sessionmaker(bind=engine)
